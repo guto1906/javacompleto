e0d47c3865f0dcd5c6fdefe5f12369eaaca47f22
Augusto Soares <guto1906@gmail.com>
2018-03-17 15:20:11 -0300

    Criação do Projeto

---
+++ src/main/java/com/guto1906/javacompleto/JavacompletoApplication.java
@@ -0,0 +1,12 @@
+package com.guto1906.javacompleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacompletoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/guto1906/javacompleto/JavacompletoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guto1906.javacompleto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavacompletoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
