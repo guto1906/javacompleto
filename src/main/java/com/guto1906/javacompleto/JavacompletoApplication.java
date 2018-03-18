d0778247ba3cb7a70e361198a85ad915a46e5a3b
Augusto Soares <guto1906@gmail.com>
2018-03-18 12:35:01 -0300

    Instanciando categorias ininiciais automaticamente

--- src/main/java/com/guto1906/javacompleto/JavacompletoApplication.java
+++ src/main/java/com/guto1906/javacompleto/JavacompletoApplication.java
@@ -1,12 +1,32 @@
 package com.guto1906.javacompleto;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.guto1906.javacompleto.domain.Categoria;
+import com.guto1906.javacompleto.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class JavacompletoApplication {
+public class JavacompletoApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavacompletoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
