f933f91601efd9326b13df10c2b66f658d40ed03
Augusto Soares <guto1906@gmail.com>
2018-03-19 23:03:33 -0300

    Aula 17 e 18-Cliente, TipoCliente, Endereco

--- src/main/java/com/guto1906/javacompleto/JavacompletoApplication.java
+++ src/main/java/com/guto1906/javacompleto/JavacompletoApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.guto1906.javacompleto.domain.Categoria;
 import com.guto1906.javacompleto.domain.Cidade;
+import com.guto1906.javacompleto.domain.Cliente;
+import com.guto1906.javacompleto.domain.Endereco;
 import com.guto1906.javacompleto.domain.Estado;
 import com.guto1906.javacompleto.domain.Produto;
+import com.guto1906.javacompleto.domain.enums.TipoCliente;
 import com.guto1906.javacompleto.repositories.CategoriaRepository;
 import com.guto1906.javacompleto.repositories.CidadeRepository;
+import com.guto1906.javacompleto.repositories.ClienteRepository;
+import com.guto1906.javacompleto.repositories.EnderecoRepository;
 import com.guto1906.javacompleto.repositories.EstadoRepository;
 import com.guto1906.javacompleto.repositories.ProdutoRepository;
 
@@ -28,6 +33,11 @@ public class JavacompletoApplication implements CommandLineRunner {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavacompletoApplication.class, args);
@@ -35,17 +45,27 @@ public class JavacompletoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		//Instanciar objetos categoria e produtos do pacote Domain
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Mouse", 80.00);		
+		Produto p3 = new Produto(null, "Mouse", 80.00);	
 		
+		//Relacionar produtos e Categorias usando o Domain
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+				
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		//Adicionar os objetos criados na base de dados usando Repository(DAO)
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
+		//Instanciar os objetos Estados e Cidade do pacote Domain
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
 		
@@ -54,18 +74,27 @@ public class JavacompletoApplication implements CommandLineRunner {
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
 		
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+		//Relacionar estados com cidades usando o Domain a relação cidade estado foi associada no construtor da cidade
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
+		//Adicionar os objetos criados na base de dados usando Repository(DAO)
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFÍSICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+	
 		
 		
 		
---
+++ src/main/java/com/guto1906/javacompleto/domain/enums/TipoCliente.java
@@ -0,0 +1,39 @@
+package com.guto1906.javacompleto.domain.enums;
+
+public enum TipoCliente {
+	
+	PESSOAFÍSICA(1, "Pessoa Física"),
+	PESSOAJURÍDICA(2, "Pessoa Jurídica");
+	
+	private Integer cod;
+	private String descricao;
+	
+	private TipoCliente(Integer cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public Integer getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		for (TipoCliente c : TipoCliente.values()) {
+			if (cod.equals(c.getCod())) {
+				return c;
+			}
+		}
+		throw new IllegalArgumentException("Código Inválido: " + cod);
+		
+	}
+	
+	
+
+}
