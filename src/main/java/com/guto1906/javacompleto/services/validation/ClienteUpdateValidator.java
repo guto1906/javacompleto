ae39e86e9010003aaa95e2d961e5de5794f645b0
Augusto Soares <guto1906@gmail.com>
2018-04-08 22:38:22 -0300

    Validacao de email repetido inserção de cliente

---
+++ src/main/java/com/guto1906/javacompleto/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,63 @@
+package com.guto1906.javacompleto.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.guto1906.javacompleto.domain.Cliente;
+import com.guto1906.javacompleto.dto.ClienteDTO;
+import com.guto1906.javacompleto.repositories.ClienteRepository;
+import com.guto1906.javacompleto.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {		
+	}
+	
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>)request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage())
+			.addPropertyNode(e.getFieldName()).addConstraintViolation();
+		}
+		
+		return list.isEmpty();
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
