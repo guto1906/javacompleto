af8ffd89138fa5d50fe2b5e920257e75f6f900d7
Augusto Soares <guto1906@gmail.com>
2018-04-24 22:49:47 -0300

    Implementando toString de pedido

--- src/main/java/com/guto1906/javacompleto/services/PedidoService.java
+++ src/main/java/com/guto1906/javacompleto/services/PedidoService.java
@@ -28,10 +28,15 @@ public class PedidoService {
 	private PagamentoRepository pagamentoRepository;
 			
 	@Autowired
-	private ProdutoService ProdutoService;
+	private ProdutoService produtoService;
 		
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	
 	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = dao.findById(id);
@@ -44,6 +49,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -56,12 +62,13 @@ public class PedidoService {
 		
 		for(ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.00);
-			ip.setPreco(ProdutoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		
 		itemPedidoRepository.saveAll(obj.getItens());
-		
+		System.out.println(obj);
 		return obj;
 		
 		
